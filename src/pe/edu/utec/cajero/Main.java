919ce25a31e2f6166ed94bfad7ba379b8c9f2c46
cpaztrillo@gmail.com <cpaztrillo@gmail.com>
2022-09-19 14:56:11 -0500

    texto

--- src/pe/edu/utec/cajero/Main.java
+++ src/pe/edu/utec/cajero/Main.java
@@ -1,5 +1,9 @@
 package pe.edu.utec.cajero;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,18 +12,21 @@ import java.util.Scanner;
 public class Main {
 
     public static Map<String, Tarjeta> initDB() {
+        Map<String, Tarjeta> baseDeDatos = cargar();
+        if(baseDeDatos!=null)
+            return baseDeDatos;
         Cuenta c1 = new Cuenta(Moneda.SOLES, new BigDecimal(200), "089-298");
         Cuenta c2 = new Cuenta(Moneda.DOLARES, new BigDecimal(100), "087-673");
         Cliente cl1 = new Cliente("Christian", "Paz", "40859458");
         cl1.creaCuenta(c1);
         cl1.creaCuenta(c2);
         Tarjeta t1 = new Tarjeta(cl1, "98765432", 1234);
-        Map<String, Tarjeta> baseDeDatos = new HashMap<>();
+        baseDeDatos = new HashMap<>();
         baseDeDatos.put(t1.numero, t1);
         return baseDeDatos;
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Map<String, Tarjeta> baseDeDatos = initDB();
         Scanner input = new Scanner(System.in);
         System.out.println("Bienvenido a su banco, digite su numero de tarjeta o 0 para terminar");
@@ -47,13 +54,14 @@ public class Main {
                     i++;
                     System.out.println("Para su cuenta: "+ cuenta.toString()+ " digite "+i);
                 }
-                System.out.println("Seleccione la cuenta para ver el saldo o 0 para salir:");
-                int idxcuenta = 0;
+                System.out.println("Seleccione la cuenta o digite 0 para salir:");
+                int idxcuenta;
                 do {
                     idxcuenta = input.nextInt();
                     if(idxcuenta>0 && idxcuenta <= t.persona.cuentas.size()) {
                         Cuenta c = t.persona.cuentas.get(idxcuenta - 1);
                         System.out.println("El saldo de su cuenta es: " + c.saldo + " " + c.moneda);
+
                     } else {
                         System.out.println("Seleccione una cuenta");
                     }
@@ -62,5 +70,34 @@ public class Main {
             fin = true;
         }
         System.out.println("Gracias por usar el sistema.");
+        grabar(baseDeDatos);
+
+    }
+
+    private static Map<String, Tarjeta> cargar() {
+        try {
+            FileInputStream fileInputStream
+                    = new FileInputStream("yourfile.txt");
+            ObjectInputStream objectInputStream
+                    = new ObjectInputStream(fileInputStream);
+            Map<String, Tarjeta> bd = (Map<String, Tarjeta>) objectInputStream.readObject();
+            objectInputStream.close();
+            return bd;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    private static void grabar(Map<String, Tarjeta> db) {
+        try {
+            FileOutputStream fileOutputStream
+                    = new FileOutputStream("yourfile.txt");
+            ObjectOutputStream objectOutputStream
+                    = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(db);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
